0e00c05b574f639e6bced0da8aae6e0b65d08941
Mussorable <redduck5601@gmail.com>
2023-07-06 17:52:12 +0200

    added validation error handler, simple request handler

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-requested-With, Content-Type, Accept"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Authorization");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+    return res.status(200).json({});
+  }
   next();
 });
 app.use("/feed", feedRoutes);
--- controllers/feed.ctr.js
+++ controllers/feed.ctr.js
@@ -1,19 +1,41 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
+        _id: "1290hv83480",
         title: "First Post",
         content: "This is the first post!",
+        imageUrl: "images/pluginIcon.png",
+        creator: {
+          name: "Olek",
+        },
+        createdAt: new Date(),
       },
     ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(402)
+      .json({ message: "Validation error!", error: errors.toArray() });
+  }
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
     message: "Post created successfully!",
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: {
+        name: "Olek",
+      },
+      createdAt: new Date(),
+    },
   });
 };
