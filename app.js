569fcee087c27c7dd2373317faadf15c5fac2be2
Mussorable <redduck5601@gmail.com>
2023-07-09 11:16:16 +0200

    added adding new post using request to the api

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const feedRoutes = require("./routes/feed.route");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -19,5 +22,16 @@ app.use((req, res, next) => {
   next();
 });
 app.use("/feed", feedRoutes);
+app.use((error, req, res, next) => {
+  console.error(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
-app.listen(5173);
+mongoose
+  .connect(
+    "mongodb+srv://Mussorable:qChxEQN1Q6rlS4Mg@cluster0.i83bgon.mongodb.net/messages?"
+  )
+  .then(() => app.listen(5173))
+  .catch((error) => console.error(error));
--- controllers/feed.ctr.js
+++ controllers/feed.ctr.js
@@ -1,41 +1,70 @@
 const { validationResult } = require("express-validator");
+const Post = require("../models/post.model");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1290hv83480",
-        title: "First Post",
-        content: "This is the first post!",
-        imageUrl: "images/pluginIcon.png",
-        creator: {
-          name: "Olek",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: "Fetched posts successfully!",
+        posts,
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(402)
-      .json({ message: "Validation error!", error: errors.toArray() });
+    const error = new Error("Validation error!");
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: {
-        name: "Olek",
-      },
-      createdAt: new Date(),
+  const post = new Post({
+    title,
+    content,
+    imageUrl: "images/duck.jpg",
+    creator: {
+      name: "Olek",
     },
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post!");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "Post fetched!", post });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next();
+    });
 };
