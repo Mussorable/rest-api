0d39583deedf7750b692a643bf8797df2013f84d
Mussorable <redduck5601@gmail.com>
2023-07-11 22:13:09 +0200

    added signup

--- controllers/feed.ctr.js
+++ controllers/feed.ctr.js
@@ -2,18 +2,27 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post.model");
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find().skip((currentPage - 1) * perPage);
+    })
     .then((posts) => {
       res.status(200).json({
         message: "Fetched posts successfully!",
         posts,
+        totalItems,
       });
     })
     .catch((error) => {
       if (!error.statusCode) {
         error.statusCode = 500;
       }
-      next(error);
+      next();
     });
 };
 
